a4cc821a0d1069115daf9f686a597b13d323f87d
Nikhitha Lakkoju <nikhithad061@gmail.com>
2026-06-03 11:31:49 +0530

    Create weight conversion(kg to pounds).py

---
+++ weight conversion(kg to pounds).py
@@ -0,0 +1,5 @@
+# Write a program that asks the user for a weight in kilograms and converts it to pounds. 
+w=float(input("Enter weight in kilograms "))
+Enter weight in kilograms 5.0
+pounds=2.205*w
+print(f"{w} in pounds are {pounds}")
